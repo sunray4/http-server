20cd6730b9b67f301fef7087a7361e17a07688c7
sunray4 <sunray4@users.noreply.github.com>
2025-07-18 16:42:55 -0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -2,38 +2,44 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	"net"
+	"os"
+	"strings"
 )
 
 func main() {
 
-	// l, err := net.Listen("tcp", "0.0.0.0:4221")
-	// if err != nil {
-	// 	fmt.Println("Failed to bind to port 4221")
-	// 	os.Exit(1)
-	// }
-	
-	// conn, err := l.Accept()
-	// if err != nil {
-	// 	fmt.Println("Error accepting connection: ", err.Error())
-	// 	os.Exit(1)
-	// }
-	// fmt.Println("Accepted connection from: ", conn.RemoteAddr())
-
-	// conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-
-	http.HandleFunc("/", handleRoot)
-	
-	http.ListenAndServe(":4221", nil)
-
-}
-
-func handleRoot(w http.ResponseWriter, req *http.Request) {
-	// fmt.Println(req.URL)
-	if req.URL.Path == "/" {
-		fmt.Fprintf(w, "HTTP/1.1 200 OK\r\n\r\n")
-	} else {
-		fmt.Fprintf(w,"HTTP/1.1 400 Not Found\r\n\r\n")
+	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	if err != nil {
+		fmt.Println("Failed to bind to port 4221")
+		os.Exit(1)
 	}
 	
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+	fmt.Println("Accepted connection from: ", conn.RemoteAddr())
+
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+
+	if (err != nil) {
+		fmt.Println("error receiving data:", err)
+	}
+
+	req := string(buf[:n])
+
+	// splits the request 
+	parts := strings.Split(req, "\r\n")
+
+	// 
+	sec := strings.Split(parts[0], " ")
+
+	if (sec[1] == "/") {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	}
 }
