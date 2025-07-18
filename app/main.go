0e67875aa3b579b2b6dfc9794af4b54a5a48009c
sunray4 <sunray4@users.noreply.github.com>
2025-07-18 16:56:52 -0700

    working server - can process request and give custom response

--- app/main.go
+++ app/main.go
@@ -31,14 +31,14 @@ func main() {
 
 	req := string(buf[:n])
 
-	// splits the request 
+	// splits the request in the request line, headers and body
 	parts := strings.Split(req, "\r\n")
 
-	// 
+	// split the request line into the http method, request target and html version
 	sec := strings.Split(parts[0], " ")
 
 	if (sec[1] == "/") {
-		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n")) // respond to the request
 	} else {
 		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 	}
