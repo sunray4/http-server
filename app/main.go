9caf105bceecf7ff0e3fc9866ae68f5d4313d652
sunray4 <sunray4@users.noreply.github.com>
2025-07-18 17:25:34 -0700

    server responds with body content

--- app/main.go
+++ app/main.go
@@ -37,7 +37,7 @@ func main() {
 	parts := strings.Split(req, "\r\n")
 
 	// split the request line into the http method, request target and html version
-	sec := strings.Split(parts[0], " ")
+	sec := strings.Split(parts[0], " ") 
 
 	if (sec[1] == "/") {
 		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n")) // respond to the request
