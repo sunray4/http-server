89f4f6f041b5597f93cca9a9e271e58ede36585d
sunray4 <sunray4@users.noreply.github.com>
2025-07-19 18:36:10 -0700

    receive post request and write files

--- app/main.go
+++ app/main.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"net"
 	"os"
-	"path/filepath"
 	"strings"
 )
 
@@ -50,14 +49,23 @@ func handleConnection(conn net.Conn) {
 	// split the request line into the http method, request target and html version
 	sec := strings.Split(parts[0], " ") 
 
-	if (sec[1] == "/") {
+	if sec[0] == "GET" {
+		handleGet(conn, parts, sec)
+	} else if sec[0] == "POST" {
+		handlePost(conn, parts, sec)
+	}
+	
+}
+
+func handleGet(conn net.Conn, parts []string, sec []string) {
+	if sec[1] == "/" {
 		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n")) // respond to the request
-	} else if (strings.Contains(sec[1], "/files/")) {
+	} else if strings.Contains(sec[1], "/files/") {
 		filename := strings.Split(sec[1], "/")[2]
-		// path, err := fileSearch("/private/tmp", filename)
+		// path, err := fileSearch(".", filename)
 		path := fmt.Sprintf("/tmp/data/codecrafters.io/http-server-tester/%s", filename)
 		_, err := os.Stat(path)
-		if (err != nil) {
+		if err != nil {
 			fmt.Print("error:", err)
 			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 		} else {
@@ -69,10 +77,10 @@ func handleConnection(conn net.Conn) {
 			conn.Write([]byte(res) ) // respond to the request
 		}
 		
-	} else if (sec[1] == "/user-agent") {
+	} else if sec[1] == "/user-agent" {
 		id := 0
 		for i := 0; i < len(parts); i++ {
-			if (strings.Contains(parts[i], "User-Agent")) {
+			if strings.Contains(parts[i], "User-Agent") {
 				id = i;
 				break;
 			}
@@ -80,7 +88,7 @@ func handleConnection(conn net.Conn) {
 		resBody := strings.Split(parts[id], " ")[1]
 		res := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(resBody), resBody)
 		conn.Write([]byte(res) )
-	} else if (strings.Contains(sec[1], "/echo/")) {
+	} else if strings.Contains(sec[1], "/echo/") {
 		res := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(sec[1][6:]), sec[1][6:])
 		conn.Write([]byte(res) ) // respond to the request
 	} else {
@@ -88,26 +96,45 @@ func handleConnection(conn net.Conn) {
 	}
 }
 
-func fileSearch(root string, target string) (string, error) {
-	var found string
-	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-		if err != nil  {
-			return err
+func handlePost(conn net.Conn, parts []string, sec []string) {
+	if strings.Contains(sec[1], "/files") {
+		filename := strings.Split(sec[1], "/")[2]
+		filepath := fmt.Sprintf("/tmp/data/codecrafters.io/http-server-tester/%s", filename)
+		content := []byte(parts[len(parts) - 1])
+
+		err := os.WriteFile(filepath, content, 0664)
+
+		if err!= nil {
+			fmt.Println("error writing file:", err)
+			conn.Write(([]byte("HTTP/1.1 500 Internal Server Error\r\n\r\n")))
+			return
 		}
-		if !info.IsDir() && info.Name() == target {
-			found = path
-			// return filepath.SkipDir
-		}
-		if !info.IsDir() {
-			fmt.Println(info.Name())
-		}
-		return err
-	})
-	if err != nil {
-		return "", err
-	} else if found == "" {
-		return "", fmt.Errorf("file not found")
-	} else {
-		return found, nil
+
+		conn.Write(([]byte("HTTP/1.1 201 Created\r\n\r\n")))
+
 	}
 }
+
+// func fileSearch(root string, target string) (string, error) {
+// 	var found string
+// 	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+// 		if err != nil  {
+// 			return err
+// 		}
+// 		if !info.IsDir() && info.Name() == target {
+// 			found = path
+// 			// return filepath.SkipDir
+// 		}
+// 		if !info.IsDir() {
+// 			fmt.Println(info.Name())
+// 		}
+// 		return err
+// 	})
+// 	if err != nil {
+// 		return "", err
+// 	} else if found == "" {
+// 		return "", fmt.Errorf("file not found")
+// 	} else {
+// 		return found, nil
+// 	}
+// }
